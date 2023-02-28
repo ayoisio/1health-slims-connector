78656e7dd7b7857845f01905200a96e85f4d93e8
Ayo Adedeji <67317859+ayoisio@users.noreply.github.com>
2023-02-27 19:38:38 -0800

    Adding steps for fetching and submitting results to 1health

--- cookbook/new_order_flow.py
+++ cookbook/new_order_flow.py
@@ -1,11 +1,11 @@
-import json
 from onehealthintegration.slims import SlimsConnector
+
 
 # Step - Create SlimsConnector instance
 slims_connector = SlimsConnector(requisition=None, verbose=True)
 
 # Step - Load requisition data
-slims_connector.load_requisition_from_file("../testdata/requisition_3066253.txt")
+slims_connector.load_requisition_from_file("testdata/requisition_3128114.txt")
 
 # Step - Create SLIMS order
 slims_connector.create_slims_order(auto_link_samples=True)
@@ -13,3 +13,12 @@ slims_connector.create_slims_order(auto_link_samples=True)
 # Step - Update SLIMS order
 updated_order_data = {}
 slims_connector.update_slims_order(updated_order_data)
+
+# Step - Fetch results only (matching filter(s)) (e.g by result status and container name)
+results = slims_connector.fetch_slims_results(result_status="Verified",  container_name="MAF21E31")
+
+# Step - Fetch results only (matching filter(s)) (e.g by test label)
+results = slims_connector.fetch_slims_results(result_status="Verified", test_label="IFN-1 Test")
+
+# Step - Fetch and submit results (matching filter(s))
+slims_connector.submit_slims_results_to_1health(result_status="Verified",  container_name="MAF21E31", dry_run=False)
