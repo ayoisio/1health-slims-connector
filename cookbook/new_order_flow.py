ad72a6f85c60dfac1f6572707b79b07400d3cd40
Ayo Adedeji <67317859+ayoisio@users.noreply.github.com>
2023-03-10 20:51:49 -0800

    Update new_order_flow.py

--- cookbook/new_order_flow.py
+++ cookbook/new_order_flow.py
@@ -14,11 +14,11 @@ slims_connector.create_slims_order(auto_link_samples=True, ignore_if_order_alrea
 updated_order_data = {}
 slims_connector.update_slims_order(updated_order_data)
 
-# Step - Fetch results only (matching filter(s)) (e.g by result status and container name)
-results = slims_connector.fetch_slims_results(result_status="Verified",  container_name="MAF21E31")
+# Step - Fetch results only (matching filter(s)) (e.g by container name)
+results = slims_connector.fetch_slims_results(container_name="MAF21E31")
 
 # Step - Fetch results only (matching filter(s)) (e.g by test label)
-results = slims_connector.fetch_slims_results(result_status="Verified", test_label="IFN-1 Test")
+results = slims_connector.fetch_slims_results(test_label="IFN-1 Test")
 
 # Step - Fetch and submit results (matching filter(s))
 slims_connector.submit_slims_results_to_1health(container_name="MAF21E31", dry_run=False)
